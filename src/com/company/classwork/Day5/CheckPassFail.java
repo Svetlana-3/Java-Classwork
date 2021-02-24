ae7fb23ddff005f9b2b71a38599fe49f94a4ad73
Svetlana-3 <svetlana.zjamkina@gmail.com>
2021-02-24 19:57:40 +0200

    CheckPassFail done

--- src/com/company/classwork/Day5/CheckPassFail.java
+++ src/com/company/classwork/Day5/CheckPassFail.java
@@ -1,8 +1,19 @@
 package com.company.classwork.Day5;
 
+import java.util.Scanner;
+
 public class CheckPassFail {
 
     public static void main(String[] args) {
-	// write your code here
+	int atz;
+	Scanner scanner = new Scanner(System.in);
+	System.out.println("Ievadiet lūdzu savu atzīmi: ");
+	atz = scanner.nextInt();
+	scanner.close();
+	if (atz >= 50) {
+		System.out.println("PASS");
+	} else {
+	        System.out.println("FAIL");
+        }
     }
-}
+    }
