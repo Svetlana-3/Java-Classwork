04022f24a1382b50eb133534ff4848227166bb5c
Svetlana-3 <svetlana.zjamkina@gmail.com>
2021-02-27 12:31:53 +0200

    Java-Classwork Calculator done

---
+++ src/com/company/classwork/Day6/Calculator.java
@@ -0,0 +1,33 @@
+package com.company.classwork.Day6;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ievadiet a: ");
+        float a = scanner.nextFloat();
+        System.out.println("Ievadiet b: ");
+        float b = scanner.nextFloat();
+        scanner.nextLine();
+
+        System.out.println("Ievadiet darbību (+,-,*,/): ");
+        String action = scanner.nextLine();
+        //System.out.println(action);
+        //switch
+
+        switch (action){
+            case "+":
+                    System.out.println(a+b);
+            break;
+            case "-":
+                System.out.println(a-b);
+                break;
+            case "*":
+                    System.out.println(a*b);
+                break;
+
+        }
+
+    }
+}
