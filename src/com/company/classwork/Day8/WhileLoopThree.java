4453b9763a096989faea7adc4dbb2e302a6c3ccb
Svetlana-3 <svetlana.zjamkina@gmail.com>
2021-03-03 19:58:36 +0200

    Klasses darbi Day8

--- src/com/company/classwork/Day8/WhileLoopThree.java
+++ src/com/company/classwork/Day8/WhileLoopThree.java
@@ -1,4 +1,22 @@
 package com.company.classwork.Day8;
 
+import java.util.Scanner;
+
 public class WhileLoopThree {
+    public static void main(String[] args){
+        System.out.println("Ievadiet ciparu: ");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
+
+        int i = 1;
+        while (i<=n){
+            System.out.printf("%d * %d = %d\n", n, i, n*i++);
+            //System.out.println(i++);
+        }
+
+
+    }
+
+
 }
--- src/com/company/classwork/Day8/WhileLoopTwo.java
+++ src/com/company/classwork/Day8/WhileLoopTwo.java
@@ -1,4 +1,16 @@
 package com.company.classwork.Day8;
 
+import java.util.Scanner;
+
 public class WhileLoopTwo {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        scanner.close();
+        int i = 0;
+        while (i<text.length()){
+            System.out.println(Character.toUpperCase(text.charAt(i++)));
+        }
+
+    }
 }
